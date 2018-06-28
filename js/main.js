af844ef9605b2c07f3e25d6cc93919dfe0277c1b
Hesham el sahhar <hesham.elsahhar@gmail.com>
2018-06-28 18:30:04 +0200

    Fixing some issues related to Accesibility

--- js/main.js
+++ js/main.js
@@ -169,11 +169,10 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.alt = restaurant.alt;
-  console.log(image.alt);
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
